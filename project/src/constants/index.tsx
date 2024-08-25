51ed91be368215b166ab28d73040b3cf93a44eeb
OnlineSourceS <115408642+CodeWith-HAMZA@users.noreply.github.com>
2024-08-25 22:54:38 +0500

    .

--- project/src/constants/index.tsx
+++ project/src/constants/index.tsx
@@ -1,7 +1,9 @@
 export const streamAppId = "1331043"
 export const streamApiKey = 'drrn9y66e39h'
 export const streamApiSecret = 'zdr5bvkz65x6a6uy5gnjmhhwj57rcdpuzk8a6kqpjy3a52jkvxrf6ev9ynjfmjbx'
-
+// NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_cHJpbWUtZ2xvd3dvcm0tNzkuY2xlcmsuYWNjb3VudHMuZGV2JA
+// CLERK_SECRET_KEY=sk_test_jHUsE05xRQQ60zGCzSRflMNfJUuXqzw25gUdhtITSM
+// NEXT_PUBLIC_BASE_URL=http://localhost:3000
 export const sidebarLinks = [
   {
     imgURL: '/icons/Home.svg',
